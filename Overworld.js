9abf8c32d7f6a1720ff5f973582c77c284b9003b
pauline-ann <paulinebantayan@gmail.com>
2022-11-21 14:59:10 -0800

    draw sprites onto map using GameObject class

--- Overworld.js
+++ Overworld.js
@@ -14,39 +14,22 @@ class Overworld {
         }
         image.src = "/images/maps/DemoLower.png" // path to image that we want to load
 
-        const x = 5
-        const y = 6
+        // Place some Game Objects!
+        const hero = new GameObject({
+            x: 6,
+            y: 6
+        })
 
-        const shadow = new Image()
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0, // left cut
-                0, // top cut
-                32, // width of cut
-                32, // height of cut
-                x * 16 - 8, // x position of hero
-                y * 16 - 18, // y position of hero
-                32, // scale/size of character on map
-                32, 
-            )
-        }
-        shadow.src = "/images/characters/shadow.png"
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/images/characters/people/npc1.png"
+        })
 
-        const hero = new Image()
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0, // left cut
-                0, // top cut
-                32, // width of cut
-                32, // height of cut
-                x * 16 - 8, // x position of hero
-                y * 16 - 18, // y position of hero
-                32, // scale/size of character on map
-                32, 
-            )
-        }
-        hero.src = "/images/characters/people/hero.png"
+        setTimeout(()=>{
+            hero.sprite.draw(this.ctx)
+            npc1.sprite.draw(this.ctx)
+        }, 200)
+       
     }
 }
