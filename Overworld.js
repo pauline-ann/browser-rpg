31a49a1c54760a84ab19943f8f28000843cf44d0
pauline-ann <paulinebantayan@gmail.com>
2022-11-21 17:06:24 -0800

    start game loop; draw maps and game objects

--- Overworld.js
+++ Overworld.js
@@ -2,33 +2,39 @@
 // Sends state downward to sub components
 class Overworld {
   constructor(config) {
-    this.element = config.element; // pass in element to overworld to operate on
-    this.canvas = this.element.querySelector(".game-canvas"); // reference canvas
-    this.ctx = this.canvas.getContext("2d"); // draw to context of canvas, give us access to drawing methods that exist on canvas elements
+    this.element = config.element // pass in element to  overworld to operate on
+    this.canvas = this.element.querySelector(".game-canvas") // reference canvas
+    this.ctx = this.canvas.getContext("2d") // draw to context of canvas, give us access to drawing methods that exist on canvas elements
+    this.map = null
+  }
+
+  startGameLoop() {
+    const step = () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+      // Draw lower layer
+      this.map.drawLowerImage(this.ctx)
+
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        // object.x += 0.02
+        object.sprite.draw(this.ctx)
+      })
+
+      // Draw upper layer
+      this.map.drawUpperImage(this.ctx)
+
+      requestAnimationFrame(() => {
+        step() // step function calls step() again when a new frame starts. not an app breaking infinite loop - other processes may still run
+      })
+    }
+    step()
   }
 
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0); // this call back is fired and image pixels are copied onto canvas
-    };
-    image.src = "/images/maps/DemoLower.png"; // path to image that we want to load
-
-    // Place some Game Objects!
-    const hero = new GameObject({
-      x: 6,
-      y: 6,
-    });
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: "/images/characters/people/npc1.png",
-    });
-
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    // create new instance of OverworldMap, passing over config from specific map, in this case DemoRoom
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen)
+    this.startGameLoop()
   }
 }
