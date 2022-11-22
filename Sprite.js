323a5e1d2edb7a73ce054ee39c91e693c8421458
pauline-ann <paulinebantayan@gmail.com>
2022-11-21 19:43:33 -0800

    update sprite animation

--- Sprite.js
+++ Sprite.js
@@ -22,13 +22,43 @@ class Sprite {
     this.animations = config.animations || {
       // default
       // each animation has a name and a series of frames inside of an array
-      idleDown: [[0, 0]],
+      // define all walking and standing animations
+      "idle-down": [
+        [0, 0]
+      ],
+      "walk-down": [
+        [1,0], [0,0], [3,0], [0,0]
+      ]
     }
-    this.currentAnimation = config.currentAnimation || "idleDown"
+    this.currentAnimation = "walk-down" //config.currentAnimation || "idle-down"
     this.currentAnimationFrame = 0 // which array within array of animation frames are we displaying
+
+    this.animationFrameLimit = config.animationFrameLimit || 16 // how many game loop frames do we want to show ie) cadence\
+    this.animationFrameProgress = this.animationFrameLimit
 
     // Reference the game object (what created sprite)
     this.gameObject = config.gameObject
+  }
+
+  // in charge of keeping track of current animation and animation frame
+  get frame() {
+    return this.animations[this.currentAnimation][this.currentAnimationFrame]
+  }
+
+  updateAnimationProgress () {
+    // downtick frame progress
+    if (this.animationFrameProgress > 0) {
+      this.animationFrameProgress -= 1
+      return
+    }
+
+    // reset counter if no more frame progress
+    this.animationFrameProgress = this.animationFrameLimit
+    this.currentAnimationFrame += 1
+
+    if (this.frame === undefined) {
+      this.currentAnimationFrame = 0
+    }
   }
 
   // this call back is fired and image pixels are copied onto canvas
@@ -38,12 +68,14 @@ class Sprite {
 
     this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
 
+    const [frameX, frameY] = this.frame
+
     // draw sprite only once sprite is loaded
     this.isLoaded &&
       ctx.drawImage(
         this.image,
-        0, // left cut
-        0, // top cut
+        frameX * 32, // left cut
+        frameY * 32, // top cut
         32, // width of cut
         32, // height of cut
         x, // x position of hero
@@ -51,5 +83,7 @@ class Sprite {
         32, // scale/size of character on map
         32
       )
+
+      this.updateAnimationProgress()
   }
 }
