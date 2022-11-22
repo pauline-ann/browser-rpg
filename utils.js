92dd79a25811aec5f640f4f07237500448f9274f
pauline-ann <paulinebantayan@gmail.com>
2022-11-22 14:28:05 -0800

    add util for dispatching custom events

--- utils.js
+++ utils.js
@@ -19,5 +19,11 @@ const utils = {
             y += size
         }
         return { x, y }
+    },
+    emitEvent(name, detail) {
+        const event = new CustomEvent(name, {
+            detail
+        })
+        document.dispatchEvent(event)
     }
 }
