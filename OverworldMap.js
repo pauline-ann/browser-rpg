812e4964cda33d43d12cd37fe2da85e89156a1a6
pauline-ann <paulinebantayan@gmail.com>
2022-11-22 14:57:24 -0800

    handle cutscene on overworld map

--- OverworldMap.js
+++ OverworldMap.js
@@ -47,6 +47,21 @@ class OverworldMap {
     })
   }
 
+  async startCutscene(events) {
+    this.isCutscenePlaying = true
+
+    // start loop of async events
+    for (let i = 0; i < events.length; i++) {
+      const eventHandler = new OverworldEvent({
+        event: events[i],
+        map: this
+      })
+      await eventHandler.init()
+    }
+
+    this.isCutscenePlaying = false
+  }
+
   // functions below help with moving characters on screen
   addWall(x, y) {
     this.walls[`${x},${y}`] = true
