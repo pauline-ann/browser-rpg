dded5a29a449345596f763e4a9992ea318d94d49
pauline-ann <paulinebantayan@gmail.com>
2022-11-22 23:33:32 -0800

    init revealing text

---
+++ RevealingText.js
@@ -0,0 +1,48 @@
+class RevealingText {
+    constructor(config) {
+        this.element = config.element
+        this.text = config.text
+        this.speed = config.speed || 80
+
+        this.timeout = null
+        this.isDone = false // jump to end of text when enter key hit, but if done, move on
+    }
+
+    // recursively reveal each character
+    revealOneCharacter(list) {
+        const next = list.splice(0, 1)[0]
+        next.span.classList.add("revealed")
+
+        if (list.length > 0) {
+            this.timeout = setTimeout(() => {
+                this.revealOneCharacter(list)
+            }, next.delayAfter)
+        } else {
+            this.isDone = true
+        }
+
+
+    }
+
+    init() {
+        let characters = []
+        this.text.split("").forEach((char) => {
+
+            // create each span, add to element in DOM
+            let span = document.createElement("span")
+            span.textContent = char
+            this.element.appendChild(span)
+
+            const charIsSpace = char === " "
+            const delayAfter = charIsSpace ? 0 : this.speed
+
+            // add this span to our internal state array
+            characters.push({
+                span,
+                delayAfter
+            })
+        })
+
+        this.revealOneCharacter(characters)
+    }
+}
