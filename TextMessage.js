2e42ef09a7cfdc9e7cbc912418abebdf36f78f60
pauline-ann <paulinebantayan@gmail.com>
2022-11-22 23:33:49 -0800

    implement typewriter effect

--- TextMessage.js
+++ TextMessage.js
@@ -10,9 +10,15 @@ class TextMessage {
         this.element.classList.add("TextMessage")
 
         this.element.innerHTML = (`
-            <p class="TextMessage_p">${this.text}</p>
+            <p class="TextMessage_p"></p>
             <button class ="TextMessage_button">Next</button>
         `)
+
+        // init typewriter effect
+        this.revealingText = new RevealingText({
+            element: this.element.querySelector(".TextMessage_p"),
+            text: this.text
+        })
 
         this.element.querySelector("button").addEventListener("click", () => {
             // close text message
@@ -33,5 +39,6 @@ class TextMessage {
     init(container) {
         this.createElement()
         container.appendChild(this.element)
+        this.revealingText.init()
     }
 }
