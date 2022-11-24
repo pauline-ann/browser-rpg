a3edb6e9f250f8d15b3dda2ebde52ddcbf051645
pauline-ann <paulinebantayan@gmail.com>
2022-11-24 02:38:19 -0800

    missing /

--- content/pizzas.js
+++ content/pizzas.js
@@ -11,18 +11,18 @@ window.Pizzas = {
         name: "Slice Samurai",
         type: PizzaTypes.spicy,
         src: "/images/characters/pizzas/s001.png",
-        icon: "images/icons/spicy.png"
+        icon: "/images/icons/spicy.png"
     },
     "v001": {
         name: "Call Me Kale",
         type: PizzaTypes.veggie,
         src: "/images/characters/pizzas/v001.png",
-        icon: "images/icons/veggie.png"
+        icon: "/images/icons/veggie.png"
     },
     "f001": {
         name: "Portobello Express",
         type: PizzaTypes.fungi,
         src: "/images/characters/pizzas/f001.png",
-        icon: "images/icons/fungi.png"
+        icon: "/images/icons/fungi.png"
     }
 }
