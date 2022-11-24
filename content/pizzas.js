6caec2529e01ba9d6f15844f10ef9f6985c8de95
pauline-ann <paulinebantayan@gmail.com>
2022-11-24 00:40:11 -0800

    add info objects into content directory

---
+++ content/pizzas.js
@@ -0,0 +1,28 @@
+window.PizzaTypes = {
+    normal: "normal",
+    spicy: "spicy",
+    veggie: "veggie",
+    fungi: "fungi",
+    chill: "chill"
+}
+
+window.Pizzas = {
+    "s001": {
+        name: "Slice Samurai",
+        type: PizzaTypes.spicy,
+        src: "/images/characters/pizzas/s001.png",
+        icon: "images/icons/spicy.png"
+    },
+    "v001": {
+        name: "Call Me Kale",
+        type: PizzaTypes.veggie,
+        src: "/images/characters/pizzas/v001.png",
+        icon: "images/icons/veggie.png"
+    },
+    "f001": {
+        name: "Portobello Express",
+        type: PizzaTypes.fungi,
+        src: "/images/characters/pizzas/f001.png",
+        icon: "images/icons/fungi.png"
+    }
+}
