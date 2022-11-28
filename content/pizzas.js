910f8ac34215d5f935231dba7b311fabb75ff30d
pauline-ann <paulinebantayan@gmail.com>
2022-11-28 01:05:31 -0800

    add actions to pizzas

--- content/pizzas.js
+++ content/pizzas.js
@@ -6,23 +6,27 @@ window.PizzaTypes = {
     chill: "chill"
 }
 
+// TODO make more complex
 window.Pizzas = {
     "s001": {
         name: "Slice Samurai",
         type: PizzaTypes.spicy,
         src: "/images/characters/pizzas/s001.png",
-        icon: "/images/icons/spicy.png"
+        icon: "/images/icons/spicy.png",
+        actions: ["damage1"] // id of battle actions
     },
     "v001": {
         name: "Call Me Kale",
         type: PizzaTypes.veggie,
         src: "/images/characters/pizzas/v001.png",
-        icon: "/images/icons/veggie.png"
+        icon: "/images/icons/veggie.png",
+        actions: ["damage1"]
     },
     "f001": {
         name: "Portobello Express",
         type: PizzaTypes.fungi,
         src: "/images/characters/pizzas/f001.png",
-        icon: "/images/icons/fungi.png"
+        icon: "/images/icons/fungi.png",
+        actions: ["damage1"]
     }
 }
