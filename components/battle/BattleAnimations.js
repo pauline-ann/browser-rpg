af7fcf123b969ca944a82ca8c57be048c9f1e4f6
pauline-ann <paulinebantayan@gmail.com>
2022-11-28 18:02:26 -0800

    add glob animation

--- components/battle/BattleAnimations.js
+++ components/battle/BattleAnimations.js
@@ -12,5 +12,29 @@ window.BattleAnimations = {
         // complete battle cycle when opponents collide
         await utils.wait(100)
         onComplete()
+    },
+
+    async glob(event, onComplete) {
+        const { caster } = event
+        let div = document.createElement("div")
+        div.classList.add("glob-orb")
+        div.classList.add(caster.team === "player" ? "battle-glob-right" : "battle-glob-left")
+
+        div.innerHTML = (`
+        <svg viewBox="0 0 32 32" width="32" height="32">
+            <circle cx="16" cy="16" r="16" fill="${event.color}" />
+        </svg>
+        `)
+
+        // remove class when animation is fully complete
+        div.addEventListener("animationend", () => {
+            div.remove()
+        })
+
+        // add to scene
+        document.querySelector(".Battle").appendChild(div)
+
+        await utils.wait(820)
+        onComplete()
     }
 }
