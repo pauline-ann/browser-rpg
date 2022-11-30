986d2ed6e0636f62ab186b97eaaeadd1a997d2fb
pauline-ann <paulinebantayan@gmail.com>
2022-11-29 18:22:52 -0800

    improve battle menu functionality

--- components/KeyboardMenu.js
+++ components/KeyboardMenu.js
@@ -11,15 +11,34 @@ class KeyboardMenu {
         this.element.innerHTML = this.options.map((option, index) => {
             const disabledAttr = option.isDisabled ? "disabled" : ""
             const spanContent = option.details ? option.details() : ""
+            const autoFocusAttr = index === 0 ? "autoFocus" : "" // TODO: edge case - first index is disabled
+
             return (`
                 <div class="option">
-                    <button ${disabledAttr} data-button="${index}" data-description="${option.description}">
+                    <button ${disabledAttr} ${autoFocusAttr} data-button="${index}" data-description="${option.description}">
                         ${option.label}
                     </button>
                     <span class="details">${spanContent}</span>
                 </div>
             `)
         }).join("")
+
+        this.element.querySelectorAll("button").forEach(button => {
+
+            button.addEventListener("click", () => {
+                const chosenOption = this.options[Number(button.dataset.button)]
+                chosenOption.handler()
+            })
+
+            button.addEventListener("mouseenter", () => {
+                button.focus()
+            })
+
+            button.addEventListener("focus", () => {
+                this.prevFocus = button
+                this.decriptionElementText.innerText = button.dataset.description
+            })
+        })
     }
 
     createElement() {
@@ -33,9 +52,38 @@ class KeyboardMenu {
         this.decriptionElementText = this.decriptionElement.querySelector("p") // save a reference to paragraph since we will be changing it a lot
     }
 
+    end() {
+
+        // remove menu element and description element
+        this.element.remove()
+        this.descriptionElement.remove()
+
+        // clean up bindings
+        this.up.unbind()
+        this.down.unbind()
+    }
+
     init(container) {
         this.createElement()
         container.appendChild(this.decriptionElement)
         container.appendChild(this.element)
+
+        // TODO clean up spaghetti code
+        this.up = new KeyPressListener("ArrowUp", () => {
+            const current = Number(this.prevFocus.getAttribute("data-button"))
+            const prevButton = Array.from(this.element.querySelectorAll("button[data-button]")).reverse().find(el => {
+                return el.dataset.button < current && !el.disabled
+            })
+            prevButton?.focus()
+        })
+
+        // TODO here too
+        this.down = new KeyPressListener("ArrowDown", () => {
+            const current = Number(this.prevFocus.getAttribute("data-button"))
+            const nextButton = Array.from(this.element.querySelectorAll("button[data-button]")).find(el => {
+                return el.dataset.button > current && !el.disabled
+            })
+            nextButton?.focus()
+        })
     }
 }
