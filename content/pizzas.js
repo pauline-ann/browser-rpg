b1d94d0f918446fa5f8a7726895f33c874975ce1
pauline-ann <paulinebantayan@gmail.com>
2022-11-30 01:13:10 -0800

    add battle action

--- content/pizzas.js
+++ content/pizzas.js
@@ -13,7 +13,7 @@ window.Pizzas = {
         type: PizzaTypes.spicy,
         src: "/images/characters/pizzas/s001.png",
         icon: "/images/icons/spicy.png",
-        actions: ["clumsyStatus", "damage1"] // id of battle actions
+        actions: ["saucyStatus", "clumsyStatus", "damage1"] // id of battle actions
     },
     "v001": {
         name: "Call Me Kale",
