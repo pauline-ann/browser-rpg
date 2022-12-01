971fcdb172aef5c630d71461b4443056f88fda26
pauline-ann <paulinebantayan@gmail.com>
2022-11-30 21:21:15 -0800

    init enemies db and player state

---
+++ content/enemies.js
@@ -0,0 +1,32 @@
+window.Enemies = {
+    "erio": {
+        name: "Erio",
+        src: "/images/characters/people/erio.png",
+        pizzas: {
+            "a": {
+                id: "s001",
+                maxHp: 50,
+                level: 1
+                // attackBonus: 20000 etc.
+                // can configure behavior here
+            },
+            "b": {
+                id: "s002",
+                maxHp: 50,
+                level: 1
+            }
+        }
+    },
+    "beth": {
+        name: "Beth",
+        src: "/images/characters/people/npc1.png",
+        pizzas: {
+            "a": {
+                id: "f001",
+                hp: 1, // TODO for testing purposes
+                maxHp: 50,
+                level: 1
+            }
+        }
+    }
+}
---
+++ state/PlayerState.js
@@ -0,0 +1,32 @@
+class PlayerState {
+    constructor() {
+        this.pizzas = {
+            "p1": {
+                id: "s001", // pizza id from /content/pizzas.js
+                hp: 30,
+                maxHp: 50,
+                xp: 75,
+                maxXp: 100,
+                level: 1,
+                status: { type: "saucy" }
+            },
+            "p2": {
+                id: "v001",
+                hp: 30,
+                maxHp: 50,
+                xp: 75,
+                maxXp: 100,
+                level: 1,
+                status: null
+            }
+        }
+        this.lineup = ["p1", "p2"], // TODO adjust order in menu later
+            this.items = [
+                { actionId: "item_recoverHp", instanceId: "item1" },
+                { actionId: "item_recoverHp", instanceId: "item2" },
+                { actionId: "item_recoverHp", instanceId: "item3" },
+            ]
+    }
+}
+
+window.playerState = new PlayerState()
