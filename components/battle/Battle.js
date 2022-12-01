ef2ecef64826891f7bbcd710fe2e114f11414560
pauline-ann <paulinebantayan@gmail.com>
2022-11-30 19:13:37 -0800

    init battle Team icons

--- components/battle/Battle.js
+++ components/battle/Battle.js
@@ -75,11 +75,24 @@ class Battle {
         this.createElement()
         container.appendChild(this.element)
 
+        this.playerTeam = new Team("player", "Hero")
+        this.enemyTeam = new Team("enemy", "Bully") // TODO make dynamic
+
         Object.keys(this.combatants).forEach(key => {
             let combatant = this.combatants[key]
             combatant.id = key
             combatant.init(this.element)
+
+            // add to correct team
+            if (combatant.team === "player") {
+                this.playerTeam.combatants.push(combatant)
+            } else if (combatant.team === "enemy") {
+                this.enemyTeam.combatants.push(combatant)
+            }
         })
+
+        this.playerTeam.init(this.element)
+        this.enemyTeam.init(this.element)
 
         this.turnCycle = new TurnCycle({
             battle: this,
