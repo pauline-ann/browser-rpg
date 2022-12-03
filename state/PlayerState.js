3255be770eaa768afa7f565e74e2396d687bbadc
pauline-ann <paulinebantayan@gmail.com>
2022-12-02 16:36:13 -0800

    prep player state for new game

--- state/PlayerState.js
+++ state/PlayerState.js
@@ -3,33 +3,15 @@ class PlayerState {
         this.pizzas = {
             "p1": {
                 pizzaId: "s001", // pizza id from /content/pizzas.js
-                hp: 30,
+                hp: 50,
                 maxHp: 50,
-                xp: 90,
-                maxXp: 100,
-                level: 1,
-                status: { type: "saucy" }
-            },
-            "p2": {
-                pizzaId: "v001",
-                hp: 30,
-                maxHp: 50,
-                xp: 75,
-                maxXp: 100,
-                level: 1,
-                status: null
-            },
-            "p3": {
-                pizzaId: "f001",
-                hp: 30,
-                maxHp: 50,
-                xp: 75,
+                xp: 0,
                 maxXp: 100,
                 level: 1,
                 status: null
             }
         }
-        this.lineup = ["p1", "p2"]
+        this.lineup = ["p1"]
         this.items = [
             { actionId: "item_recoverHp", instanceId: "item1" },
             { actionId: "item_recoverHp", instanceId: "item2" },
