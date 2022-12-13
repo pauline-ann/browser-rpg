ba7ff9fc22c1177a602b59651b015cecda4b67e6
pauline-ann <paulinebantayan@gmail.com>
2022-12-12 23:19:49 -0800

    add enemy battle dialogue

--- db/enemies.js
+++ db/enemies.js
@@ -2,7 +2,7 @@ window.Enemies = {
     "cypher": {
         name: "Cypher",
         src: "/db/images/characters/people/cypher.png",
-        pizzas: {
+        pizzas: {// TODO pizzas
             "a": {
                 pizzaId: "f001",
                 maxHp: 30,
@@ -13,6 +13,70 @@ window.Enemies = {
             preBattle: "The goal of the battle is to defeat all of your opponent's pizzas. You have a mix of moves and items that range between harming the enemy and protecting your own pizzas. Strategize to defeat your opponent. Good luck!",
             battleWin: "Wow... impressive.",
             battleLoss: "You need more time to prepare."
+        }
+    },
+    "eligor": {
+        name: "Eligor",
+        src: "/db/images/characters/people/eligor.png",
+        pizzas: {// TODO pizzas
+            "a": {
+                pizzaId: "f001",
+                maxHp: 30,
+                level: 1
+            }
+        },
+        textMessages: {
+            preBattle: "This better not be a waste of time...",
+            battleWin: "Whatever.",
+            battleLoss: "Hmph."
+        }
+    },
+    "kenji": {
+        name: "Kenji",
+        src: "/db/images/characters/people/kenji.png",
+        pizzas: {// TODO pizzas
+            "a": {
+                pizzaId: "f001",
+                maxHp: 30,
+                level: 1
+            }
+        },
+        textMessages: {
+            preBattle: "Let's see what you can do!",
+            battleWin: "Well done.",
+            battleLoss: "Try practicing more."
+        }
+    },
+    "lavender": {
+        name: "Lavender",
+        src: "/db/images/characters/people/lavender.png",
+        pizzas: {// TODO pizzas
+            "a": {
+                pizzaId: "f001",
+                maxHp: 30,
+                level: 1
+            }
+        },
+        textMessages: {
+            preBattle: "Don't go easy on me!",
+            battleWin: "Awwww maaaaaaan.",
+            battleLoss: "Maybe you'll do better next time!"
+        }
+    },
+    "ironChef": {
+        name: "Iron Chef",
+        src: "/db/images/characters/people/ironChef.png",
+        pizzas: {// TODO pizzas
+            "a": {
+                pizzaId: "f001",
+                maxHp: 30,
+                level: 1
+            }
+        },
+        textMessages: {
+            preBattle: "KEKEKEKEKE",
+            battleWin: "NOOOOO!",
+            battleLoss: "Mwahahahahaha!"
         }
     }
 }
